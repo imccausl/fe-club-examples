65ceb1f9d3bdad5302c8acd2ad042d1384dc8117
Jennifer Cahalane <jennifer.cahalane@tophatmonocle.com>
2021-01-06 16:22:53 -0500

    add prettier, removable pages, wire up nav tabs

--- src/App.js
+++ src/App.js
@@ -1,33 +1,101 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
+import { navTabs, pages } from "./constants";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isGlobalNavOpen: false,
-    }
+      activePage: this.getActiveTitle(),
+      activeHash: window.location.hash || "#content",
+      pagesList: Object.keys(pages),
+    };
   }
+
+  componentDidMount() {
+    window.addEventListener("hashchange", this.onHashChanged, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("hashchange", this.onHashChanged);
+  }
+
+  onHashChanged = () => {
+    this.setState({ activeHash: window.location.hash });
+  };
+
+  getActiveTitle = () => {
+    const path = window.location.pathname;
+    if (path) {
+      return path.replaceAll("/page/", "");
+    }
+    return Object.keys(pages)[0];
+  };
+
+  getContent = (content, level) => (
+    <React.Fragment>
+      {content.heading && (
+        <p className={`Content-header${level}`}>{content.heading}</p>
+      )}
+      {content.text &&
+        content.text.map((text) => <p className="Content-text">{text}</p>)}
+      {content.children &&
+        content.children.map((child) => this.getContent(child, level + 1))}
+    </React.Fragment>
+  );
+
+  openPage = (pageTitle) => {
+    this.setState({ activePage: pageTitle });
+    window.history.replaceState(
+      { pageTitle },
+      "pageTitle",
+      `/page/${pageTitle}`
+    );
+  };
+
+  deletePage = (pageTitle) => {
+    const pagesList = this.state.pagesList.filter(
+      (title) => title !== pageTitle
+    );
+    this.setState({ pagesList });
+  };
+
   render() {
-    const {isGlobalNavOpen} = this.state
+    const { isGlobalNavOpen } = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
           <div className="Header-universalheader">
             <div className="UniversalHeader-links">
-              <a href="#courses" className="UniversalHeader-linkitem">Courses</a>
-              <a href="#marketplace" className="UniversalHeader-linkitem">Marketplace</a>
+              <a href="#courses" className="UniversalHeader-linkitem">
+                Courses
+              </a>
+              <a href="#marketplace" className="UniversalHeader-linkitem">
+                Marketplace
+              </a>
             </div>
             <div className="UniversalHeader-navlist">
-              <button className="UniversalHeader-buttondropdown" onClick={() => this.setState({isGlobalNavOpen: !isGlobalNavOpen})}>Jennifer Cahalane</button>
+              <button
+                className="UniversalHeader-buttondropdown"
+                onClick={() =>
+                  this.setState({ isGlobalNavOpen: !isGlobalNavOpen })
+                }
+              >
+                Jennifer Cahalane
+              </button>
               {isGlobalNavOpen && (
                 <ul className="UniversalHeader-list">
                   <li className="UniversalHeader-listitem">
-                    <a href="/settings" className="UniversalHeader-listlink">Settings</a>
+                    <a href="/settings" className="UniversalHeader-listlink">
+                      Settings
+                    </a>
                   </li>
                   <li className="UniversalHeader-listitem">
-                    <a href="/logout" className="UniversalHeader-listlink">Log out</a>
+                    <a href="/logout" className="UniversalHeader-listlink">
+                      Log out
+                    </a>
                   </li>
                 </ul>
               )}
@@ -36,9 +104,17 @@ class App extends React.Component {
           <div className="Header-info">
             <p className="Header-title">A11y Reading Group</p>
             <div className="Header-tabs">
-              <a href="#content" className="Header-tab">Content</a>
-              <a href="#gradebook" className="Header-tab">Gradebook</a>
-              <a href="#students" className="Header-tab">Students</a>
+              {navTabs.map((tab) => {
+                let classes = "Header-tab";
+                if (tab.hash === this.state.activeHash) {
+                  classes += " Header-tab--active";
+                }
+                return (
+                  <a href={tab.hash} className={classes}>
+                    {tab.title}
+                  </a>
+                );
+              })}
             </div>
           </div>
         </header>
@@ -46,35 +122,33 @@ class App extends React.Component {
         <div className="App-body">
           <div className="App-sidebar" aria-label="sidebar: page list">
             <ul className="Sidebar-list">
-              <li className="Sidebar-listitem">
-                <a href="/page/1" className="Sidebar-link">page 1</a>
-              </li>
-              <li className="Sidebar-listitem">
-                <a href="/page/2" className="Sidebar-link">page 2</a>
-              </li>
-              <li className="Sidebar-listitem">
-                <a href="/page/3" className="Sidebar-link">page 3</a>
-              </li>
-              <li className="Sidebar-listitem">
-                <a href="/page/4" className="Sidebar-link">page 4</a>
-              </li>
+              {this.state.pagesList.map((pageTitle) => (
+                <li className="Sidebar-listitem" key={pageTitle}>
+                  <button
+                    onClick={() => this.openPage(pageTitle)}
+                    className="Sidebar-link"
+                  >
+                    {pageTitle.replaceAll("-", " ")}
+                  </button>
+                  <button
+                    className="Sidebar-deleteitem"
+                    onClick={() => this.deletePage(pageTitle)}
+                  >
+                    -
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
 
           <main className="App-content">
-            <p className="Content-header">Nullam ac Tortor Vitae</p>
-            <p className="Content-subheader">Nulla Malesuada</p>
-              <p className="Content-sub-subheader">Arcu</p>
-              <p className="Content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim neque volutpat ac tincidunt vitae semper. Turpis massa sed elementum tempus egestas sed sed. Nunc mattis enim ut tellus elementum sagittis. Aenean vel elit scelerisque mauris pellentesque pulvinar. Amet volutpat consequat mauris nunc congue nisi. Integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Interdum posuere lorem ipsum dolor. Nisi vitae suscipit tellus mauris a diam. Vestibulum rhoncus est pellentesque elit. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Molestie a iaculis at erat pellentesque adipiscing commodo elit. Lacus laoreet non curabitur gravida arcu ac tortor dignissim. Faucibus nisl tincidunt eget nullam non nisi est sit. Purus ut faucibus pulvinar elementum integer enim neque volutpat.</p>
-              <p className="Content-text">Sed libero enim sed faucibus. Aenean pharetra magna ac placerat vestibulum. Augue interdum velit euismod in pellentesque massa placerat duis ultricies. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Est placerat in egestas erat imperdiet sed euismod nisi porta. Et ligula ullamcorper malesuada proin libero nunc. Suspendisse ultrices gravida dictum fusce ut. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Turpis egestas pretium aenean pharetra magna ac. Quis varius quam quisque id diam vel. Id aliquet risus feugiat in ante metus dictum.</p>
-              <p className="Content-sub-subheader">Habitant Morbi Tristique</p>
-              <p className="Content-text">Faucibus vitae aliquet nec ullamcorper sit. Arcu dui vivamus arcu felis bibendum ut tristique. Velit ut tortor pretium viverra. Pellentesque id nibh tortor id aliquet lectus proin nibh. Nisl rhoncus mattis rhoncus urna neque viverra. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Mauris pharetra et ultrices neque. Cursus mattis molestie a iaculis at erat pellentesque. Scelerisque fermentum dui faucibus in ornare. Nunc non blandit massa enim nec dui nunc. Porta nibh venenatis cras sed felis. Velit dignissim sodales ut eu sem integer vitae justo eget. Sed odio morbi quis commodo odio aenean sed. Id volutpat lacus laoreet non. Mollis nunc sed id semper risus in hendrerit gravida.</p>
-            <p className="Content-subheader">Odio Facilisis</p>
-            <p className="Content-text">Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. Pretium viverra suspendisse potenti nullam. Luctus accumsan tortor posuere ac ut consequat semper. Congue eu consequat ac felis donec. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Nullam vehicula ipsum a arcu cursus. Congue mauris rhoncus aenean vel. Neque aliquam vestibulum morbi blandit cursus risus at ultrices. Lectus sit amet est placerat in egestas erat imperdiet sed. In fermentum posuere urna nec tincidunt praesent. Enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra. Viverra adipiscing at in tellus. Justo laoreet sit amet cursus. Interdum velit euismod in pellentesque massa placerat duis ultricies lacus. Vestibulum mattis ullamcorper velit sed. Id semper risus in hendrerit gravida rutrum quisque non. Sit amet nisl suscipit adipiscing. Sed elementum tempus egestas sed sed risus pretium quam.</p>
+            {pages[this.state.activePage].map((content) =>
+              this.getContent(content, 0)
+            )}
           </main>
         </div>
       </div>
-    )
+    );
   }
 }
 
---
+++ src/constants.js
@@ -0,0 +1,156 @@
+export const pages = {
+  "the-wonderful-wizard-of-oz": [
+    {
+      heading: "The Wonderful Wizard of Oz",
+      children: [
+        {
+          heading: "The Cyclone",
+          children: [
+            {
+              heading: "The Start",
+              text: [
+                "Dorothy lived in the midst of the great Kansas prairies, with Uncle Henry, who was a farmer, and Aunt Em, who was the farmer’s wife. Their house was small, for the lumber to build it had to be carried by wagon many miles. There were four walls, a floor and a roof, which made one room; and this room contained a rusty looking cookstove, a cupboard for the dishes, a table, three or four chairs, and the beds. Uncle Henry and Aunt Em had a big bed in one corner, and Dorothy a little bed in another corner. There was no garret at all, and no cellar—except a small hole dug in the ground, called a cyclone cellar, where the family could go in case one of those great whirlwinds arose, mighty enough to crush any building in its path. It was reached by a trap door in the middle of the floor, from which a ladder led down into the small, dark hole.",
+                "When Dorothy stood in the doorway and looked around, she could see nothing but the great gray prairie on every side. Not a tree nor a house broke the broad sweep of flat country that reached to the edge of the sky in all directions. The sun had baked the plowed land into a gray mass, with little cracks running through it. Even the grass was not green, for the sun had burned the tops of the long blades until they were the same gray color to be seen everywhere. Once the house had been painted, but the sun blistered the paint and the rains washed it away, and now the house was as dull and gray as everything else.",
+                "When Aunt Em came there to live she was a young, pretty wife. The sun and wind had changed her, too. They had taken the sparkle from her eyes and left them a sober gray; they had taken the red from her cheeks and lips, and they were gray also. She was thin and gaunt, and never smiled now. When Dorothy, who was an orphan, first came to her, Aunt Em had been so startled by the child’s laughter that she would scream and press her hand upon her heart whenever Dorothy’s merry voice reached her ears; and she still looked at the little girl with wonder that she could find anything to laugh at.",
+              ],
+            },
+            {
+              heading: "Then a Strange Thing Happened.",
+              text: [
+                "The house whirled around two or three times and rose slowly through the air. Dorothy felt as if she were going up in a balloon.",
+                "The north and south winds met where the house stood, and made it the exact center of the cyclone. In the middle of a cyclone the air is generally still, but the great pressure of the wind on every side of the house raised it up higher and higher, until it was at the very top of the cyclone; and there it remained and was carried miles and miles away as easily as you could carry a feather.",
+              ],
+            },
+          ],
+        },
+        {
+          heading: "The Council with the Munchkins",
+          text: [
+            "She was awakened by a shock, so sudden and severe that if Dorothy had not been lying on the soft bed she might have been hurt. As it was, the jar made her catch her breath and wonder what had happened; and Toto put his cold little nose into her face and whined dismally. Dorothy sat up and noticed that the house was not moving; nor was it dark, for the bright sunshine came in at the window, flooding the little room. She sprang from her bed and with Toto at her heels ran and opened the door.",
+            "The little girl gave a cry of amazement and looked about her, her eyes growing bigger and bigger at the wonderful sights she saw.",
+          ],
+        },
+      ],
+    },
+  ],
+  "peter-pan": [
+    {
+      heading: "Peter Pan",
+      children: [
+        {
+          heading: "Peter Breaks Through",
+          children: [
+            {
+              heading: "All children, except one, grow up",
+              text: [
+                "They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she must have looked rather delightful, for Mrs. Darling put her hand to her heart and cried, “Oh, why can't you remain like this for ever!” This was all that passed between them on the subject, but henceforth Wendy knew that she must grow up. You always know after you are two. Two is the beginning of the end.",
+                "Of course they lived at 14 [their house number on their street], and until Wendy came her mother was the chief one. She was a lovely lady, with a romantic mind and such a sweet mocking mouth. Her romantic mind was like the tiny boxes, one within the other, that come from the puzzling East, however many you discover there is always one more; and her sweet mocking mouth had one kiss on it that Wendy could never get, though there it was, perfectly conspicuous in the right-hand corner.",
+              ],
+            },
+            {
+              heading: "Wendy came first, then John, then Michael",
+              text: [
+                "For a week or two after Wendy came it was doubtful whether they would be able to keep her, as she was another mouth to feed. Mr. Darling was frightfully proud of her, but he was very honourable, and he sat on the edge of Mrs. Darling's bed, holding her hand and calculating expenses, while she looked at him imploringly. She wanted to risk it, come what might, but that was not his way; his way was with a pencil and a piece of paper, and if she confused him with suggestions he had to begin at the beginning again.",
+                "“Now don't interrupt,” he would beg of her.",
+              ],
+            },
+          ],
+        },
+        {
+          heading: "The Shadow",
+          text: [
+            "Mrs. Darling screamed, and, as if in answer to a bell, the door opened, and Nana entered, returned from her evening out. She growled and sprang at the boy, who leapt lightly through the window. Again Mrs. Darling screamed, this time in distress for him, for she thought he was killed, and she ran down into the street to look for his little body, but it was not there; and she looked up, and in the black night she could see nothing but what she thought was a shooting star.",
+            "She returned to the nursery, and found Nana with something in her mouth, which proved to be the boy's shadow. As he leapt at the window Nana had closed it quickly, too late to catch him, but his shadow had not had time to get out; slam went the window and snapped it off.",
+            "You may be sure Mrs. Darling examined the shadow carefully, but it was quite the ordinary kind.",
+          ],
+        },
+      ],
+    },
+  ],
+  "alice-in-wonderland": [
+    {
+      heading: "Alice in Wonderland",
+      children: [
+        {
+          heading: "Down the Rabbit-Hole",
+          children: [
+            {
+              heading: "Alice was beginning to get very tired",
+              text: [
+                "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’",
+                "So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her.",
+                "There was nothing so VERY remarkable in that; nor did Alice think it so VERY much out of the way to hear the Rabbit say to itself, ‘Oh dear! Oh dear! I shall be late!’ (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually TOOK A WATCH OUT OF ITS WAISTCOAT-POCKET, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge.",
+              ],
+            },
+            {
+              heading: "The rabbit-hole",
+              text: [
+                "The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well.",
+                "Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next. First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and bookshelves; here and there she saw maps and pictures hung upon pegs. She took down a jar from one of the shelves as she passed; it was labelled ‘ORANGE MARMALADE’, but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody, so managed to put it into one of the cupboards as she fell past it.",
+              ],
+            },
+          ],
+        },
+        {
+          heading: "The Pool of Tears",
+          text: [
+            "‘Curiouser and curiouser!’ cried Alice (she was so much surprised, that for the moment she quite forgot how to speak good English); ‘now I’m opening out like the largest telescope that ever was! Good-bye, feet!’ (for when she looked down at her feet, they seemed to be almost out of sight, they were getting so far off). ‘Oh, my poor little feet, I wonder who will put on your shoes and stockings for you now, dears? I’m sure I shan’t be able! I shall be a great deal too far off to trouble myself about you: you must manage the best way you can; –but I must be kind to them,’ thought Alice, ‘or perhaps they won’t walk the way I want to go! Let me see: I’ll give them a new pair of boots every Christmas.’",
+          ],
+        },
+      ],
+    },
+  ],
+  "around-the-world-in-80-days": [
+    {
+      heading: "Around the World in 80 Days",
+      children: [
+        {
+          heading:
+            "In which Phileas Fogg and Passepartout Accept Each Other, the One as Master, the Other as Man",
+          children: [
+            {
+              heading: "Mr. Phileas Fogg lived",
+              text: [
+                "Mr. Phileas Fogg lived, in 1872, at No. 7, Saville Row, Burlington Gardens, the house in which Sheridan died in 1814.  He was one of the most noticeable members of the Reform Club, though he seemed always to avoid attracting attention; an enigmatical personage, about whom little was known, except that he was a polished man of the world.  People said that he resembled Byron--at least that his head was Byronic; but he was a bearded, tranquil Byron, who might live on a thousand years without growing old.",
+                "Certainly an Englishman, it was more doubtful whether Phileas Fogg was a Londoner.  He was never seen on 'Change, nor at the Bank, nor in the counting-rooms of the 'City'; no ships ever came into London docks of which he was the owner; he had no public employment; he had never been entered at any of the Inns of Court, either at the Temple, or Lincoln's Inn, or Gray's Inn; nor had his voice ever resounded in the Court of Chancery, or in the Exchequer, or the Queen's Bench, or the Ecclesiastical Courts.  He certainly was not a manufacturer; nor was he a merchant or a gentleman farmer.  His name was strange to the scientific and learned societies, and he never was known to take part in the sage deliberations of the Royal Institution or the London Institution, the Artisan's Association, or the Institution of Arts and Sciences.  He belonged, in fact, to none of the numerous societies which swarm in the English capital, from the Harmonic to that of the Entomologists, founded mainly for the purpose of abolishing pernicious insects.",
+              ],
+            },
+            {
+              heading:
+                "Phileas Fogg was a member of the Reform, and that was all.",
+              text: [
+                "The way in which he got admission to this exclusive club was simple enough.",
+                "He was recommended by the Barings, with whom he had an open credit. His cheques were regularly paid at sight from his account current, which was always flush.",
+                "Was Phileas Fogg rich?  Undoubtedly.  But those who knew him best could not imagine how he had made his fortune, and Mr. Fogg was the last person to whom to apply for the information.  He was not lavish, nor, on the contrary, avaricious; for, whenever he knew that money was needed for a noble, useful, or benevolent purpose, he supplied it quietly and sometimes anonymously.  He was, in short, the least communicative of men.  He talked very little, and seemed all the more mysterious for his taciturn manner.  His daily habits were quite open to observation; but whatever he did was so exactly the same thing that he had always done before, that the wits of the curious were fairly puzzled.",
+              ],
+            },
+          ],
+        },
+        {
+          heading:
+            "In Which Passepartout is Convinced That he has at Last Found His Ideal",
+          text: [
+            "'Faith,' muttered Passepartout, somewhat flurried, 'I've seen people at Madame Tussaud's as lively as my new master!'",
+            "Madame Tussaud's 'people,' let it be said, are of wax, and are much visited in London; speech is all that is wanting to make them human.",
+            "During his brief interview with Mr. Fogg, Passepartout had been carefully observing him.  He appeared to be a man about forty years of age, with fine, handsome features, and a tall, well-shaped figure; his hair and whiskers were light, his forehead compact and unwrinkled, his face rather pale, his teeth magnificent.  His countenance possessed in the highest degree what physiognomists call 'repose in action,' a quality of those who act rather than talk.  Calm and phlegmatic, with a clear eye, Mr. Fogg seemed a perfect type of that English composure which Angelica Kauffmann has so skilfully represented on canvas.  Seen in the various phases of his daily life, he gave the idea of being perfectly well-balanced, as exactly regulated as a Leroy chronometer. Phileas Fogg was, indeed, exactitude personified, and this was betrayed even in the expression of his very hands and feet; for in men, as well as in animals, the limbs themselves are expressive of the passions.",
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export const navTabs = [
+  {
+    hash: "#content",
+    title: "content",
+  },
+  {
+    hash: "#gradebook",
+    title: "gradebook",
+  },
+  {
+    hash: "#students",
+    title: "students",
+  },
+];
