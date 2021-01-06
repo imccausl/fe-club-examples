bc481a1c949d4344994239044434d6e33af21983
Jennifer Cahalane <jennifer.cahalane@tophatmonocle.com>
2021-01-06 18:07:53 -0500

    fixing broken class names

--- src/Components/Page/index.js
+++ src/Components/Page/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import "./Page.css";
+import React from 'react'
+import './Page.css'
 
 export const Page = ({ content, level }) => (
   <React.Fragment>
-    <p className={`page--heading--${level}`}>{content.heading}</p>
+    <p className={`Page--heading--${level}`}>{content.heading}</p>
     {content.text &&
-      content.text.map((text) => (
-        <p key={text.substring(10)} className="page--text">
+      content.text.map(text => (
+        <p key={text.substring(10)} className="Page--text">
           {text}
         </p>
       ))}
     {content.children &&
-      content.children.map((child) => (
+      content.children.map(child => (
         <Page
           key={child.heading.substring(10)}
           content={child}
@@ -19,4 +19,4 @@ export const Page = ({ content, level }) => (
         />
       ))}
   </React.Fragment>
-);
+)
