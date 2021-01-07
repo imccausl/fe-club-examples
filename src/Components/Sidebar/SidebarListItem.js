a5354e9e36abacd328d6e14452b9f85a7b368a9c
Ian McCausland <imccausl@gmail.com>
2021-01-06 22:35:06 -0500

    add button with tooltip

--- src/Components/Sidebar/SidebarListItem.js
+++ src/Components/Sidebar/SidebarListItem.js
@@ -1,10 +1,16 @@
+import { ButtonWithTooltip } from '../Tooltip'
+
 export const SidebarListItem = ({ title, openPage, deletePage }) => (
   <li className="Sidebar--item" key={title.substring(10)}>
     <button onClick={() => openPage(title)} className="Sidebar--button">
-      {title.replaceAll("-", " ")}
+      {title.replaceAll('-', ' ')}
     </button>
-    <button className="Sidebar--delete" onClick={() => deletePage(title)}>
+    <ButtonWithTooltip
+      className="Sidebar--delete"
+      onClick={() => deletePage(title)}
+      tooltipContent="Delete item!"
+    >
       -
-    </button>
+    </ButtonWithTooltip>
   </li>
-);
+)
--- src/Components/Tooltip/index.js
+++ src/Components/Tooltip/index.js
@@ -1,37 +1,69 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import './Tooltip.css'
 
 export const InfoIconTooltip = ({ info }) => {
-    const [showTooltip, setShowTooltip] = useState(false)
+  const [isVisible, setIsVisible] = useState(false)
 
-    const toggleShowTooltip = () => {
-        setShowTooltip(true)
+  const toggleShowTooltip = () => {
+    setIsVisible(true)
+  }
+
+  const toggleHideTooltip = () => {
+    setIsVisible(false)
+  }
+
+  return (
+    <div className="Tooltip--container">
+      <div
+        className="Tooltip--button"
+        role="button"
+        onMouseOver={toggleShowTooltip}
+        onMouseLeave={toggleHideTooltip}
+      >
+        ℹ️
+      </div>
+      {isVisible && <div className="TooltipIcon--content">{info}</div>}
+    </div>
+  )
+}
+
+export const withTooltip = (WrappedButtonComponent) =>
+  class extends React.Component {
+    state = {
+      isVisible: false,
     }
 
-    const toggleHideTooltip = () => {
-        setShowTooltip(false)
+    handleShowTooltip = () => {
+      this.setState({
+        isVisible: true,
+      })
     }
 
-    return (
+    handleHideTooltip = () => {
+      this.setState({
+        isVisible: false,
+      })
+    }
+
+    render() {
+      const { tooltipContent, ...restProps } = this.props
+      console.log(restProps.children)
+      return (
         <div className="Tooltip--container">
-            <div
-                className="Tooltip--button"
-                role="button"
-                onMouseOver={toggleShowTooltip}
-                onMouseLeave={toggleHideTooltip}
-            >
-                ℹ️
-            </div>
-            {showTooltip && (
-                <div className="Tooltip--content">
-                    {info}
-                </div>
-            )}
+          <WrappedButtonComponent
+            {...restProps}
+            onMouseOver={this.handleShowTooltip}
+            onMouseLeave={this.handleHideTooltip}
+          />
+          {this.state.isVisible && (
+            <div className="Tooltip--content">{tooltipContent}</div>
+          )}
         </div>
-    )
-}
+      )
+    }
+  }
 
-export const Tooltip = WrappedComponent => ({content}) => {
+const Button = (props) => <button {...props} />
 
-}
+export const ButtonWithTooltip = withTooltip(Button)
